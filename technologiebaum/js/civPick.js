86ca4fec4bf690ec3de22ef376eaa61b7b367330
derechtenap <50623315+derechtenap@users.noreply.github.com>
2020-01-11 19:59:59 +0100

    Design 0.1

--- technologiebaum/js/civPick.js
+++ technologiebaum/js/civPick.js
@@ -1,19 +1,35 @@
-var azteken = {
+var azt = {
     "id": 1,
     "name": "Azteken",
-    "toString": function() {
-        return this.name;
-    }
+    "typ": "Volk von Mönchen",
+    "bonus": "...",
+    "uu": "...",
+    "team_bonus": "..."
+}
+
+var ber = {
+    "id": 2,
+    "name": "Berber",
 }
 
 function changeCiv() {
+    var civ;
     var selectBox = document.getElementById("selectBox");
     var selectedValue = selectBox.options[selectBox.selectedIndex].value;
-    // alert(selectedValue);
 
-    if (selectedValue === "azteken") {
-        document.getElementById("civ-name").innerHTML = azteken;
-    } else {
-        document.getElementById("civ-name").innerHTML = "Kultur";
+    switch(selectedValue) {
+        case "azt":
+            civ = azt.name;
+            break;
+        case "ber":
+            civ = ber.name;
+            break;
+        case "bir":
+            civ = bir.name;
+            break;
+        // Weitere Civs fehlen noch
     }
+    
+    document.getElementById("civ-name").innerHTML = civ;
+
 }
